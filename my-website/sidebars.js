6e51889c10a326df40165539d46bfda9d361d4a4
jhtep <john@epvisual.com>
2020-07-06 14:33:29 -0400

    Sub1

--- my-website/sidebars.js
+++ my-website/sidebars.js
@@ -1,6 +1,11 @@
 module.exports = {
   someSidebar: {
-    'Docusaurus Head': ['doc1', 'doc2', 'doc3'],
+    'Docusaurus Head': [
+      'doc1',
+      'doc2',
+      'doc3',
+      { Sub1: ['Sub1/sub1', 'Sub1/sub2'] },
+    ],
     FeaturesX: ['mdx'],
   },
 };
